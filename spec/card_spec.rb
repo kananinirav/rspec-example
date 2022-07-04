f01f8b835ae1c463cb120d23627bc6a5ef2b3cbb
kananinirav <30398499+kananinirav@users.noreply.github.com>
2022-07-04 15:25:38 +0900

    code refactoring

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,7 +10,7 @@ class Card
 end
 
 describe Card do
-  let(:card) { Card.new('Nirav', 'Kanani') } # only initialize when required
+  let(:card) { Card.new('Nirav', 'Kanani') } # only initialize when required (lazy-loaded)
   # let!(:card) { Card.new('Nirav', 'Kanani') } # Before each test case initialize same as before block
   # let(:x) { 1 + 1 }
   # let(:y) { x + 10 }
@@ -19,7 +19,6 @@ describe Card do
   # before do
   #   @card = Card.new('Nirav', 'Kanani')
   # end
-   
   it 'has a rank and that rank can change' do
     expect(card.rank).to eq('Nirav')
     card.rank = 'Raj'
@@ -30,7 +29,7 @@ describe Card do
     expect(card.suit).to eq('Kanani')
   end
 
-  # If we want to display custom error message 
+  # If we want to display custom error message
   it 'has a custom error message' do
     card.suit = 'Nonsense'
     comparison = 'Kanani'
